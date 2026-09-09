bf80307184ab4036523278eb3a0bc1da268b45f6
DavidSiepermann <dsiepermann@gmx.de>
2026-09-09 12:09:38 +0200

    Update main.py
    Co-Authored-By: j17792897341j <326913666+j17792897341j@users.noreply.github.com>
    Co-Authored-By: ValentinSchuler <190609298+valentinschuler@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,5 @@
+# Mögliche Klassen:
+# - User -> Atrribute username, rateversuche
+# Methoden: registrieren, einloggen, spiel starten, 
+# - Game -> Attribute gewonnen, verloren, rateversuche
+# Methoden: spielen, beenden, statistik anzeigen
